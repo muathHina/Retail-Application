af30efb533eca34a78a18666a6536d4e0fba44fc
JorBloke <muath.hina@gmail.com>
2012-04-22 17:58:00 +0200

    Update application/config/config.php

--- application/config/config.php
+++ application/config/config.php
@@ -224,7 +224,7 @@ $config['cache_path'] = '';
 | MUST set an encryption key.  See the user guide for info.
 |
 */
-$config['encryption_key'] = '93101fcJby4VeMLf0666DwcK5Q7gx9Q8';
+$config['encryption_key'] = '';
 
 /*
 |--------------------------------------------------------------------------
